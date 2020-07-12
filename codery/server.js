c84dc971ca472ce5e505b667da900cb0703b0b7e
kobelev.ilya@gmail.com <Ilya.Kobelev@acronis.com>
2020-07-12 22:33:08 +0300

    hw unit 5.4

--- codery/server.js
+++ codery/server.js
@@ -1,20 +1,57 @@
-// config parameters
+// server config params
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const http = require('http');
+const url = require("url");
 let counter = 0;
 
 const server = http.createServer(function (req, res) {
-  console.log("Request, url:", req.url);
+  switch(url.parse(req.url).pathname) {
+    case "/":
+      serveIndex(res);
+      break;
+    case "/reset":
+      serveReset(res)
+      break;
+    case "/counter":
+      serveCounter(res)
+      break;
+    default:
+      serveNotFound(res);
+  }
+});
+
+function serveIndex(res) {
+  initResponse(res);
+  counter++;
+  res.write("Main page");
+  res.end();
+}
+
+function serveCounter(res) {
+  initResponse(res);
+  res.write(`Значение счетчика: ${counter}`);
+  res.end();
+}
+
+function serveReset(res) {
+  initResponse(res);
+  counter = 0;
+  res.write("Счетчик сброшен");
+  res.end();
+}
+
+function serveNotFound(res) {
+  initResponse(res);
+  res.write("Not found");
+  res.end();
+}
+
+function initResponse(res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain;charset=utf-8");
-  if (req.url != "/favicon.ico") {
-    counter++;
-  }
-  res.write(`Номер запроса: ${counter}`);
-  res.end();
-});
+}
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}`)
