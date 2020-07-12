4df86489038874c467d8f82860df40d6f719c3c1
kobelev.ilya@gmail.com <Ilya.Kobelev@acronis.com>
2020-07-12 18:13:35 +0300

    unit 5.3 fixed after review

--- codery/server.js
+++ codery/server.js
@@ -1,15 +1,21 @@
+// config parameters
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const http = require('http');
 let counter = 0;
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function (req, res) {
   console.log("Request, url:", req.url);
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain;charset=utf-8");
-  if (req.url!="/favicon.ico") {
+  if (req.url != "/favicon.ico") {
     counter++;
   }
   res.write(`Номер запроса: ${counter}`);
   res.end();
 });
 
-server.listen(8080);
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`)
+});
