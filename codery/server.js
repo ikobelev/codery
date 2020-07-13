d23611b09443586fdffc583456c6d956dbaaa707
kobelev.ilya@gmail.com <Ilya.Kobelev@acronis.com>
2020-07-13 22:02:24 +0300

    hw 5.5 fixed after review

--- codery/server.js
+++ codery/server.js
@@ -1,62 +1,82 @@
-// server config params
+// Параметры конфигурации сервера
 const hostname = '127.0.0.1';
 const port = 3000;
+const urlReset = '/reset';
+const urlCounter = '/counter';
 
 const http = require('http');
-const url = require("url");
+const url = require('url');
+
 let counter = 0;
 
-const server = http.createServer(function (req, res) {
-  switch(url.parse(req.url).pathname) {
-    case "/":
+/**
+ * Вспомогательный метод инициализации ответа
+ */
+function initResponse(res, statusCode) {
+  res.statusCode = statusCode;
+  res.setHeader('Content-Type', 'text/html;charset=utf-8');
+}
+
+/**
+ * Главная страница
+ */
+function serveIndex(res) {
+  initResponse(res, 200);
+  counter += 1;
+  const html = '<ul>'
+    + `<li><a href="${urlCounter}">Счетчик</li>`
+    + `<li><a href="${urlReset}"/>Сброс</li>`
+    + '</ul>';
+  res.write(html);
+  res.end();
+}
+
+/**
+ * Вывод страницы со счетчиком
+ */
+function serveCounter(res) {
+  initResponse(res, 200);
+  res.write(`Значение счетчика: ${counter}`);
+  res.end();
+}
+
+/**
+ * Сброс счетчика
+ */
+function serveReset(res) {
+  initResponse(res, 200);
+  counter = 0;
+  res.write('Счетчик сброшен');
+  res.end();
+}
+
+/**
+ * Вывод страницы 404
+ */
+function serveNotFound(res) {
+  initResponse(res, 404);
+  res.write('Not found');
+  res.end();
+}
+
+// Роутинг запроса
+const server = http.createServer((req, res) => {
+  switch (url.parse(req.url).pathname) {
+    case '/':
       serveIndex(res);
       break;
-    case "/reset":
-      serveReset(res)
+    case urlReset:
+      serveReset(res);
       break;
-    case "/counter":
-      serveCounter(res)
+    case urlCounter:
+      serveCounter(res);
       break;
     default:
       serveNotFound(res);
   }
 });
 
-function serveIndex(res) {
-  initResponse(res);
-  counter++;
-  const html =  "<ul>" +
-                "<li><a href=\"/counter\"/>Счетчик</li>" +
-                "<li><a href=\"/reset\"/>Сброс</li>" +
-                "</ul>";
-  res.write(html);
-  res.end();
-}
-
-function serveCounter(res) {
-  initResponse(res);
-  res.write(`Значение счетчика: ${counter}`);
-  res.end();
-}
-
-function serveReset(res) {
-  initResponse(res);
-  counter = 0;
-  res.write("Счетчик сброшен");
-  res.end();
-}
-
-function serveNotFound(res) {
-  initResponse(res);
-  res.write("Not found");
-  res.end();
-}
-
-function initResponse(res) {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html;charset=utf-8");
-}
-
+// Запуск сервера
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`)
+  console.log(`Server running at http://${hostname}:${port}`);
 });
