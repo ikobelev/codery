a305209cb160ff1c0ff6b2f322c2bc1be874663b
kobelev.ilya@gmail.com <Ilya.Kobelev@acronis.com>
2020-07-16 23:13:41 +0300

    hw 5.6 after review

--- codery/server.js
+++ codery/server.js
@@ -8,13 +8,38 @@ const url = require('url');
 const fs = require('fs');
 
 /**
+ * Вспомогательная функция вывода заданного текстового контента
+ */
+function sendContent(res, statusCode, textContent) {
+  res.statusCode = statusCode;
+  if (textContent) {
+    res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+    res.write(textContent);
+  }
+  res.end();
+}
+
+/**
+ * Вывод страницы 404
+ */
+function serveNotFound(res) {
+  sendContent(res, 404, 'Страница не найдена');
+}
+
+/**
  * Вывод статического контента
  */
 function serveStatic(req, res, customFileName) {
-  const filename = customFileName || path.basename(req.url);
-  const extension = path.extname(filename);
+  const fileName = customFileName || path.basename(req.url);
 
-  switch (extension) {
+  // проверяем наличие файла
+  const filePath = `static/${fileName}`;
+  if (!fs.existsSync(filePath)) {
+    serveNotFound();
+  }
+
+  // выдаем контент файла
+  switch (path.extname(fileName)) {
     case '.html':
       res.setHeader('Content-Type', 'text/html;charset=utf-8');
       break;
@@ -26,28 +51,25 @@ function serveStatic(req, res, customFileName) {
       break;
     default:
   }
-  const content = fs.readFileSync(`static/${filename}`);
-  res.statusCode = 200;
-  res.write(content);
-  res.end();
-}
-
-/**
- * Вывод страницы 404
- */
-function serveNotFound(res) {
-  res.statusCode = 404;
-  res.end();
+  // читаем и выдаем контент файла в потоке
+  const stream = fs.createReadStream(filePath);
+  stream.pipe(res);
 }
 
 // Роутинг запроса
 const server = http.createServer((req, res) => {
-  if (url.parse(req.url).pathname === '/') {
-    serveStatic(req, res, 'index.html');
-  } else if (path.dirname(req.url) === '/static') {
-    serveStatic(req, res);
-  } else {
-    serveNotFound(res);
+  try {
+    if (url.parse(req.url).pathname === '/') {
+      serveStatic(req, res, 'index.html');
+    } else if (path.dirname(req.url) === '/static') {
+      serveStatic(req, res);
+    } else {
+      serveNotFound(res);
+    }
+  } catch (e) {
+    // что-то пошло не так
+    console.log(`Ошибка при обработке запроса: ${e}`);
+    sendContent(res, 500, 'Что-то пошло не так');
   }
 });
 
