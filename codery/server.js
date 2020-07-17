1a9ec78548e2b0619b854980aa6b1654443056ba
kobelev.ilya@gmail.com <Ilya.Kobelev@acronis.com>
2020-07-17 22:40:15 +0300

    hw 5.6 rev.2

--- codery/server.js
+++ codery/server.js
@@ -35,7 +35,8 @@ function serveStatic(req, res, customFileName) {
   // проверяем наличие файла
   const filePath = `static/${fileName}`;
   if (!fs.existsSync(filePath)) {
-    serveNotFound();
+    serveNotFound(res);
+    return;
   }
 
   // выдаем контент файла
